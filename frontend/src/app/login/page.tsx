358e200853c8c29f792d63083c61a64f8aa8ad00
Tvinki228 <pes-log@ukr.net>
2024-12-30 13:56:34 +0100

    Create login page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -2,15 +2,33 @@ import styles from "./login.module.scss";
 
 export default function LoginPage() {
     return (
-        <div className={styles.test}>
-            <h1>Login</h1>
-            <form>
-                <label>Username:</label>
-                <input type="text" placeholder="Enter username" />
-                <label>Password:</label>
-                <input type="password" placeholder="Enter password" />
-                <button type="submit">Login</button>
-            </form>
-        </div>
+        <section className={styles["login-page"]}>
+            <div className={styles["login-page__container"]}>
+                <form className={styles["login-page__form"]}>
+                    <label className={styles["login-page__label"]}>
+                        Username
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="Enter username"
+                        className={`${styles["login-page__input"]} ${styles["login-page__input--username"]}`}
+                    />
+                    <label className={styles["login-page__label"]}>
+                        Password:
+                    </label>
+                    <input
+                        type="password"
+                        placeholder="Enter password"
+                        className={`${styles["login-page__input"]} ${styles["login-page__input--password"]}`}
+                    />
+                    <button
+                        type="submit"
+                        className={`${styles["login-page__button"]} ${styles["login-page__button--submit"]}`}
+                    >
+                        Log In
+                    </button>
+                </form>
+            </div>
+        </section>
     );
 }

7dcd175e13f12024138e22fe473f67f7474aeff4
Tvinki228 <pes-log@ukr.net>
2025-01-02 11:45:39 +0100

    Add logic for event page

--- frontend/src/app/events/[id]/page.tsx
+++ frontend/src/app/events/[id]/page.tsx
@@ -1,10 +1,39 @@
+"use client";
+
+import { useParams } from "next/navigation";
 import styles from "./event.module.scss";
 
+const events = {
+    1: {
+        title: "ROCK AM RING",
+        date: "JUNE 2025",
+        location: "Nürburgring, Germany",
+        description: "One of the biggest rock festivals in the world.",
+    },
+    2: {
+        title: "SUMMER FESTIVAL",
+        date: "JULY 2025",
+        location: "Berlin, Germany",
+        description: "A vibrant summer music festival in Berlin.",
+    },
+};
+
 export default function EventDetailPage() {
+    const params = useParams();
+    const { id } = params;
+
+    const event = events[id as keyof typeof events];
+
+    if (!event) {
+        return <p>Event not found!</p>;
+    }
+
     return (
-        <div>
-            <h1>Event Details</h1>
-            <p>Here you can see the details for the selected event.</p>
+        <div className={styles["event-details"]}>
+            <h1>{event.title}</h1>
+            <p>Date:{event.date}</p>
+            <p>Location: {event.location}</p>
+            <p>{event.description}</p>
         </div>
     );
 }
--- frontend/src/app/events/page.tsx
+++ frontend/src/app/events/page.tsx
@@ -1,6 +1,41 @@
 import EventCard from "@/components/EventCard/EventCard";
 import styles from "./events.module.scss";
 
+const events = [
+    {
+        id: "1",
+        title: "ROCK AM RING",
+        date: "JUNE 2025",
+        location: "Nürburgring, Germany",
+        description: "One of the biggest rock festivals in the world.",
+        image: "/images/attendees-at-concert-event.jpg",
+    },
+    {
+        id: "2",
+        title: "SUMMER FESTIVAL",
+        date: "JULY 2025",
+        location: "Berlin, Germany",
+        description: "A vibrant summer music festival in Berlin.",
+        image: "/images/summer-festival.jpg",
+    },
+    {
+        id: "3",
+        title: "SUMMER FESTIVAL",
+        date: "JULY 2025",
+        location: "Berlin, Germany",
+        description: "A vibrant summer music festival in Berlin.",
+        image: "/images/summer-festival.jpg",
+    },
+    {
+        id: "4",
+        title: "SUMMER FESTIVAL",
+        date: "JULY 2025",
+        location: "Berlin, Germany",
+        description: "A vibrant summer music festival in Berlin.",
+        image: "/images/summer-festival.jpg",
+    },
+];
+
 export default function EventsPage() {
     return (
         <section className={styles["events-page"]}>
@@ -9,12 +44,9 @@ export default function EventsPage() {
                     Upcoming Events
                 </h1>
                 <div className={styles["events-page__cards"]}>
-                    <EventCard />
-                    <EventCard />
-                    <EventCard />
-                    <EventCard />
-                    <EventCard />
-                    <EventCard />
+                    {events.map((event) => (
+                        <EventCard key={event.id} eventId={event.id} />
+                    ))}
                 </div>
             </div>
         </section>
